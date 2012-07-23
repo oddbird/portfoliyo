4caf7d88a14c2301679579f551589cbb111fdb3c
Carl Meyer <carl@oddbird.net>
2012-07-23 13:52:50 -0600

    Friendlier error messages on missing/invalid email.

--- portfoliyo/landing/forms.py
+++ portfoliyo/landing/forms.py
@@ -9,3 +9,12 @@ class LeadForm(forms.ModelForm):
         model = Lead
         fields = ["email"]
         widgets = {"email": forms.EmailInput}
+
+
+    def __init__(self, *args, **kwargs):
+        """Override some error messages."""
+        super(LeadForm, self).__init__(*args, **kwargs)
+
+        error_msg = "That doesn't look like an email address; double-check it?"
+        self.fields["email"].error_messages["required"] = error_msg
+        self.fields["email"].error_messages["invalid"] = error_msg
