f03d6a8fb5089a76eee875f4a0a94d8db38108d5
Carl Meyer <carl@oddbird.net>
2012-07-25 09:35:24 -0600

    Tweak lead admin list page.

--- portfoliyo/landing/admin.py
+++ portfoliyo/landing/admin.py
@@ -9,6 +9,10 @@ class LeadAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": [("email", "following_up"), "notes", "signed_up"]})]
     readonly_fields = ["signed_up"]
+    list_filter = ["following_up"]
+    date_hierarchy = "signed_up"
+    list_display = ["email", "following_up", "signed_up", "notes"]
+    list_editable = ["following_up"]
 
 
 
