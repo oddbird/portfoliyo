1e64b75fbab855ee78aa1ba0bf2776015433adc2
Jonny Gerig Meyer <jonny@oddbird.net>
2012-08-08 15:22:24 -0400

    initial user_edit template

--- portfoliyo/alpha_urls.py
+++ portfoliyo/alpha_urls.py
@@ -4,10 +4,15 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns(
     "",
     url(
-        "^$",
+        r"^signup/",
         direct_to_template,
         {"template": "signup.html"},
         name="landing",
         ),
-
+    url(
+        r"^username/edit/",
+        direct_to_template,
+        {"template": "users/user_edit.html"},
+        name="user_edit",
+        ),
 )
