b8906ad52856fa36960b5e49ed415be7a993a240
Jonny Gerig Meyer <jonny@oddbird.net>
2012-08-20 18:01:57 -0400

    add defuscator

--- static/js/init.js
+++ static/js/init.js
@@ -6,6 +6,7 @@ var PYO = (function (PYO, $) {
         // plugins
         $('input[placeholder], textarea[placeholder]').placeholder();
         $('#messages').messages({handleAjax: true});
+        $('.email').defuscate();
 
         // local.js
         PYO.ajaxifyForm('.signup .container form');
