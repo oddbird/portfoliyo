1edd040af089a3eb8630c07442d6130e6af3fec6
Jonny Gerig Meyer <jonny@oddbird.net>
2012-08-23 16:59:16 -0400

    landing page ajax-form; messages

--- static/js/init.js
+++ static/js/init.js
@@ -9,7 +9,7 @@ var PYO = (function (PYO, $) {
         $('.email').defuscate();
 
         // local.js
-        PYO.ajaxifyForm('.landing .membership form');
+        PYO.ajaxifyForm('.village-landing .membership form', '.village-landing .membership');
         PYO.updatePageHeight('.village');
         PYO.updateVillageScroll('.village-feed');
     });
