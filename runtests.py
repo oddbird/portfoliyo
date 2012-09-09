eb0a6fe05a9d8b78336cc4d1974608770edcb265
Carl Meyer <carl@oddbird.net>
2012-09-09 16:47:04 -0600

    Run migrations when creating test db.

--- runtests.py
+++ runtests.py
@@ -10,6 +10,15 @@ from _pytest.core import main as pytest_main
 
 def main():
     os.environ["DJANGO_SETTINGS_MODULE"] = "portfoliyo.settings.default"
+
+    # Override pytest-django and run South migrations in tests
+    from pytest_django import plugin
+    def _enable_migrations_in_tests():
+        from south.management.commands import patch_for_test_db_setup
+        patch_for_test_db_setup()
+    plugin._disable_south_management_command = _enable_migrations_in_tests
+
+
     args = sys.argv[1:]
     if not args:
         args = ["tests/", "--cov-report", "html", "--cov", "portfoliyo"]
