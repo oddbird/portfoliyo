429f5af495a0a02b5e36554751a23292070c03e5
Carl Meyer <carl@oddbird.net>
2012-09-09 17:00:53 -0600

    Complete test coverage of landing-page app.

---
+++ tests/landing/test_views.py
@@ -0,0 +1,24 @@
+"""Tests for landing-page view."""
+from django.core.urlresolvers import reverse
+
+from portfoliyo.landing.models import Lead
+
+
+def test_signup(client):
+    """User can sign up with email; gets friendly message, stored as Lead."""
+    form = client.get(reverse("landing")).forms["landing-form"]
+    form["email"] = "foo@example.com"
+    response = form.submit().follow()
+
+    assert "Thanks for your interest" in response.body
+    assert Lead.objects.get().email == u"foo@example.com"
+
+
+def test_bad_email(client):
+    """If user submits a bad email, they get a friendlyish error."""
+    form = client.get(reverse("landing")).forms["landing-form"]
+    form["email"] = "foo"
+    response = form.submit()
+
+    assert "look like an email address" in response.body
+    assert Lead.objects.count() == 0
