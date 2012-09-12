42db1c085b3374ef9a63239dd41d0f47e61d8a40
Jonny Gerig Meyer <jonny@oddbird.net>
2012-09-12 12:13:16 -0400

    js dynamic formset for add-student add-elders form

--- static/js/init.js
+++ static/js/init.js
@@ -7,6 +7,14 @@ var PYO = (function (PYO, $) {
         $('input[placeholder], textarea[placeholder]').placeholder();
         $('#messages').messages({handleAjax: true});
         $('.email').defuscate();
+        $('.formset.elders').formset({
+            prefix: $('.formset.elders').data('prefix'),
+            formTemplate: '#empty-elder-form',
+            formSelector: '.fieldset.elder',
+            addLink: '<a class="pluselder" href="javascript:void(0)">more elders</a>',
+            addAnimationSpeed: 'normal',
+            removeAnimationSpeed: 'fast'
+        });
 
         // local.js
         PYO.ajaxifyForm('.village-landing .membership form', '.village-landing .membership');
