95966ecb443594ecb275ab9690c5e2b39dd9b28b
Carl Meyer <carl@oddbird.net>
2012-09-12 14:39:13 -0600

    Only show add-student and invite-elder buttons to school staff.

--- tests/village/test_views.py
+++ tests/village/test_views.py
@@ -1,6 +1,8 @@
 """Tests for village views."""
 from django.core.urlresolvers import reverse
 
+import pytest
+
 from tests import utils
 from tests.users import factories
 
@@ -53,3 +55,33 @@ class TestAddStudent(object):
         response = client.get(reverse('add_student'), status=302).follow()
 
         assert not "account doesn't have access" in response.content
+
+
+
+class TestChat(object):
+    """Tests for village chat view."""
+    def url(self, student=None):
+        if student is None:
+            student = factories.ProfileFactory.create()
+        return reverse('chat', kwargs=dict(student_id=student.id))
+
+
+    @pytest.mark.parametrize('link_target', ['add_student', 'invite_elder'])
+    def test__button_only_if_staff(self, client, link_target):
+        """Button with given link target is only present for school staff."""
+        parent_rel = factories.RelationshipFactory.create(
+            from_profile__school_staff=False)
+        teacher_rel = factories.RelationshipFactory.create(
+            from_profile__school_staff=True, to_profile=parent_rel.student)
+        url = self.url(parent_rel.student)
+        parent_response = client.get(url, user=parent_rel.elder.user)
+        teacher_response = client.get(url, user=teacher_rel.elder.user)
+        reverse_kwargs = {}
+        if link_target == 'invite_elder':
+            reverse_kwargs = {'student_id': parent_rel.student.id}
+        target_url = reverse(link_target, kwargs=reverse_kwargs)
+        parent_links = parent_response.html.findAll('a', href=target_url)
+        teacher_links = teacher_response.html.findAll('a', href=target_url)
+
+        assert len(teacher_links) == 1
+        assert len(parent_links) == 0
