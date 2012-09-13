2b9114a0318c70a5c785c918095e953663c87b4a
Carl Meyer <carl@oddbird.net>
2012-09-13 17:44:33 -0600

    Activate user when we get a text from them.

--- portfoliyo/village/sms.py
+++ portfoliyo/village/sms.py
@@ -1,5 +1,17 @@
 """Village SMS-handling."""
+from ..users import models
+
+
 def receive_sms(source, body):
     """Hook for when an SMS is received."""
-    # @@@ make user active
+    try:
+        profile = models.Profile.objects.select_related(
+            'user').get(phone=source)
+    except models.Profile.DoesNotExist:
+        # @@@ log this?
+        return
+
+    profile.user.is_active = True
+    profile.user.save()
+
     # @@@ post message
