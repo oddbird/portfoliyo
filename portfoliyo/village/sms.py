285290bb0cd3dff25c491e8f8202781ee4fbec94
Carl Meyer <carl@oddbird.net>
2012-09-14 02:17:05 -0600

    Create Post on receiving a text.

--- portfoliyo/village/sms.py
+++ portfoliyo/village/sms.py
@@ -1,17 +1,36 @@
 """Village SMS-handling."""
-from ..users import models
+from ..users import models as user_models
+from . import models
 
 
 def receive_sms(source, body):
-    """Hook for when an SMS is received."""
+    """
+    Hook for when an SMS is received.
+
+    Return None if no reply should be sent, or text of reply.
+
+    """
     try:
-        profile = models.Profile.objects.select_related(
+        profile = user_models.Profile.objects.select_related(
             'user').get(phone=source)
-    except models.Profile.DoesNotExist:
+    except user_models.Profile.DoesNotExist:
         # @@@ log this?
-        return
+        return None
 
     profile.user.is_active = True
     profile.user.save()
 
-    # @@@ post message
+    students = profile.students
+
+    if len(students) > 1:
+        return (
+            "You're part of more than one student's Portfoliyo.org Village; "
+            "we're not yet able to route your texts. Sorry!"
+            )
+    elif not students:
+        return (
+            "Sorry, you're not part of any student's Portfoliyo.org Village; "
+            "we're not able to deliver your message. Sorry!"
+            )
+
+    models.Post.create(profile, students[0], body)
