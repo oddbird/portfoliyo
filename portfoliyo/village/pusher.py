be4f57da6bfa47478765ab4c1cb79df8db3f38a0
Carl Meyer <carl@oddbird.net>
2012-09-14 12:03:58 -0600

    Oops; actually make things work without pusher settings.

--- portfoliyo/village/pusher.py
+++ portfoliyo/village/pusher.py
@@ -9,9 +9,9 @@ def context_processor(request):
 
 def get_pusher():
     """Return a real pusher client if configured in settings, or None."""
-    app_id = getattr(settings, 'PUSHER_APP_ID')
-    key = getattr(settings, 'PUSHER_KEY')
-    secret = getattr(settings, 'PUSHER_SECRET')
+    app_id = getattr(settings, 'PUSHER_APPID', None)
+    key = getattr(settings, 'PUSHER_KEY', None)
+    secret = getattr(settings, 'PUSHER_SECRET', None)
     if app_id and key and secret:
        return pusher.Pusher(app_id=app_id, key=key, secret=secret)
     return None
