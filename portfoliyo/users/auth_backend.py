325f7b36f90addcc22651c3f4c048e5c0fed4ae2
Carl Meyer <carl@oddbird.net>
2012-09-20 15:35:08 -0600

    Add test for EmailBackend legacy import location.

--- portfoliyo/users/auth_backend.py
+++ portfoliyo/users/auth_backend.py
@@ -4,5 +4,16 @@ Backwards-compatibility import location for EmailBackend.
 Django stores auth backend dotted paths in the session, so we need to keep this
 around until all existing sessions have expired / been renewed.
 
+Here is the code to run on the live database to determine when it is safe to
+remove this fallback:
+
+from django.contrib.sessions.models import Session
+import base64, pickle
+set([pickle.loads(base64.decodestring(s.session_data)[41:]).get('_auth_user_backend', '') for s in Session.objects.all()])
+
+If the resulting set does not contain
+'portfoliyo.users.auth_backend.EmailBackend', this module can safely be
+removed.
+
 """
 from portfoliyo.model.users.auth_backend import EmailBackend
