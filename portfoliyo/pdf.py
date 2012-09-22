7bba76279f12c2a5d56a3567da1c8c5dabb6d821
Carl Meyer <carl@oddbird.net>
2012-09-21 22:12:19 -0600

    Fix locations for code/phone/example in new PDF templates.

--- portfoliyo/pdf.py
+++ portfoliyo/pdf.py
@@ -1,11 +1,33 @@
 """PDF code."""
 import io
+import os
 
 import pyPdf
+# from reportlab.lib.fonts import addMapping
 from reportlab.lib.pagesizes import LETTER, landscape
+# from reportlab.pdfbase import pdfmetrics
+# from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 
 from portfoliyo import formats
+
+
+
+LOCATIONS = {
+    'parent-instructions-template-en.pdf': {
+        'code': (358, 225),
+        'phone': (342, 207),
+        'example': (386, 171),
+        },
+    'parent-instructions-template-es.pdf': {
+        'code': (370, 189),
+        'phone': (345, 171),
+        'example': (386, 135),
+        },
+    }
+
+
+COLOR = (7, 54, 66)
 
 
 
@@ -13,16 +35,30 @@ def generate_instructions_pdf(template_path, stream, code, phone):
     """Generate a parent signup instructions PDF and write it to stream."""
     template_page = pyPdf.PdfFileReader(open(template_path, 'rb')).getPage(0)
 
+    # font_path = os.path.join(
+    #     os.path.dirname(os.path.abspath(__file__)),
+    #     'cambridge_round_bold.ttf',
+    #     )
+
+    # pdfmetrics.registerFont(TTFont('Cambridge-Round-Bold', font_path))
+    # addMapping('Cambridge-Round-Bold', 0, 0, 'Cambridge-Round-Bold')
+
+    locations = LOCATIONS[os.path.basename(template_path)]
+    code_loc = locations['code']
+    phone_loc = locations['phone']
+    example_loc = locations['example']
+
     buffer = io.BytesIO()
 
     p = canvas.Canvas(buffer, pagesize=landscape(LETTER))
 
-    p.setFont('Helvetica-Bold', 16)
+    p.setFont('Helvetica-Bold', 11)
+    p.setFillColorRGB(*[c/255.0 for c in COLOR])
 
-    p.drawString(390, 372, code)
+    p.drawString(code_loc[0], code_loc[1], code)
     p.drawString(
-        380, 347, formats.display_phone(phone))
-    p.drawString(438, 300, code)
+        phone_loc[0], phone_loc[1], formats.display_phone(phone))
+    p.drawString(example_loc[0], example_loc[1], '%s Jane Doe' % code)
 
     p.showPage()
     p.save()
