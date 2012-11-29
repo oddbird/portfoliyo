aea5a2e6b19c165cb3d778d6bc7dc1e8799c5e1d
Carl Meyer <carl@oddbird.net>
2012-11-28 19:38:08 -0700

    Improve user/profile admins.

--- portfoliyo/model/users/admin.py
+++ portfoliyo/model/users/admin.py
@@ -1,4 +1,8 @@
+from django import forms
+
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import forms as auth_forms
 from django.contrib.auth import models as auth_models
 
 from . import models
@@ -7,38 +11,96 @@ from . import models
 class RelationshipsFromInline(admin.TabularInline):
     model = models.Relationship
     fk_name = 'to_profile'
+    extra = 0
 
 
 class RelationshipsToInline(admin.TabularInline):
     model = models.Relationship
     fk_name = 'from_profile'
+    extra = 0
 
 
-def email(profile):
-    return profile.user.email
-email.admin_order_field = 'user__email'
-
-
-admin.site.register(
-    models.Profile,
-    inlines=[RelationshipsFromInline, RelationshipsToInline],
+class ProfileAdmin(admin.ModelAdmin):
+    inlines=[RelationshipsFromInline, RelationshipsToInline]
     list_display=[
-        'school',
+        '__unicode__',
         'name',
-        email,
+        'email',
         'phone',
         'role',
+        'school',
         'school_staff',
-        'code',
-        'invited_by',
-        'declined',
-        ],
-    list_filter=['school', 'school_staff', 'declined'],
-    )
+        ]
+    list_filter=['school', 'school_staff', 'declined']
+
+
+    def email(self, profile):
+        return profile.user.email
+    email.admin_order_field = 'user__email'
+
+
+    def save_model(self, request, obj, form, change):
+        # ensure phone is never empty string
+        obj.phone = obj.phone or None
+        return super(ProfileAdmin, self).save_model(request, obj, form, change)
+
+
+admin.site.register(models.Profile, ProfileAdmin)
 admin.site.register(models.Group)
 admin.site.register(models.School)
 
 
+
+class UserChangeForm(forms.ModelForm):
+    password = auth_forms.ReadOnlyPasswordHashField(
+        help_text=
+        "Raw passwords are not stored, so there is no way to see "
+        "this user's password, but you can change the password "
+        "using <a href=\"password/\">this form</a>.",
+        )
+
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+    def clean_email(self):
+        return self.cleaned_data.get('email', None) or None
+
+
+    class Meta:
+        model = auth_models.User
+
+
+class UserAdmin(auth_admin.UserAdmin):
+    list_display = ('email', 'name', 'is_superuser', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+    form = UserChangeForm
+    fieldsets = [(None, {'fields': (
+                    'email',
+                    'password',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'last_login',
+                    'date_joined',
+                    )})]
+
+
+    def name(self, obj):
+        try:
+            return obj.profile.name
+        except models.Profile.DoesNotExist:
+            return '<no profile>'
+    name.admin_order_field = 'profile__name'
+
+
+if auth_models.User in admin.site._registry: # pragma: no cover
+    admin.site.unregister(auth_models.User)
+
+admin.site.register(auth_models.User, UserAdmin)
+
 # we don't use contrib.auth groups; confusing to have them in the admin
 if auth_models.Group in admin.site._registry: # pragma: no cover
-    admin.site.unregister(auth_models.Group)  # pragma: no cover
+    admin.site.unregister(auth_models.Group)
