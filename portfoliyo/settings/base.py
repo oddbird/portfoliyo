5fb16ead435c765feb62e31c0145b8a45803885b
Carl Meyer <carl@oddbird.net>
2012-12-07 17:01:46 -0700

    No debug logging to console by default.

--- portfoliyo/settings/base.py
+++ portfoliyo/settings/base.py
@@ -163,11 +163,6 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'portfoliyo.wsgi.application'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG is False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -177,7 +172,7 @@ LOGGING = {
         },
     },
     "root": {
-        "level": "DEBUG",
+        "level": "INFO",
         "handlers": ["console"],
     },
     "formatters": {
