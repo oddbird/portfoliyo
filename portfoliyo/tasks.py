51c49a420fc2c058b965ab145f75391cabc2e176
Carl Meyer <carl@oddbird.net>
2012-12-18 16:38:20 -0700

    Logging and sentry error-catching for Celery.

--- portfoliyo/tasks.py
+++ portfoliyo/tasks.py
@@ -1,13 +1,24 @@
 from celery import Celery
+from celery.utils.log import get_task_logger
 from django.conf import settings
+from raven.contrib.celery import register_signal
+from raven.contrib.django.models import client
+
+
+logger = get_task_logger(__name__)
+
+
+
+# automatic logging of task failures to Sentry
+register_signal(client)
+
 
 
 if settings.REDIS_URL: # pragma: no cover
     celery = Celery(
-        'tasks',
         broker=settings.REDIS_URL,
         backend=settings.REDIS_URL,
         )  # pragma: no cover
 else:
-    celery = Celery('tasks')
+    celery = Celery()
     celery.conf.update(CELERY_ALWAYS_EAGER=True)
