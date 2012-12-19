3b94b604822644c0de29a8da1a9da0576a992a91
Carl Meyer <carl@oddbird.net>
2012-12-19 10:34:29 -0700

    Add hashes and sorted sets to fake Redis; improve pipelining.

--- portfoliyo/redis.py
+++ portfoliyo/redis.py
@@ -58,20 +58,73 @@ class InMemoryRedis(object):
         return val
 
 
+    def hmset(self, key, mapping):
+        d = self.data.setdefault(key, {})
+        d.update(mapping)
+        return True
+
+
+    def hgetall(self, key):
+        return self.data.get(key, {})
+
+
+    def zadd(self, key, score, val):
+        score = float(score)
+        val = str(val)
+        l = self.data.setdefault(key, [])
+        add_index = 0
+        rem_index = None
+        for i, (iscore, ival) in enumerate(l):
+            if iscore < score:
+                add_index = i + 1
+            if val == ival:
+                rem_index = i
+        l.insert(add_index, (score, val))
+        if rem_index is not None:
+            if add_index <= rem_index:
+                rem_index += 1
+            l.pop(rem_index)
+        return 1
+
+
+    def zrangebyscore(self, key, min, max):
+        min = float(min)
+        max = float(max)
+        ret = []
+        for score, val in self.data.get(key, []):
+            if score >= min and score <= max:
+                ret.append(val)
+        return ret
+
+
     def pipeline(self):
-        """
-        Fake pipelining by just returning this instance.
+        return Pipeline(self)
 
-        This'll fail if we try to use the "chaining" feature of real redis.py
-        pipeline API. So we won't.
 
-        """
-        return self
+
+class Pipeline(object):
+    def __init__(self, client):
+        self.client = client
+        self.calls = []
 
 
     def execute(self):
-        """Do nothing; pipelining is faked and the calls already happened."""
-        pass
+        results = []
+        for method_name, args, kwargs in self.calls:
+            results.append(getattr(self.client, method_name)(*args, **kwargs))
+        return results
+
+
+def _make_pipelined_method(name):
+    def _pipelined_method(self, *args, **kwargs):
+        self.calls.append((name, args, kwargs))
+        return self
+
+    return _pipelined_method
+
+for method_name in dir(InMemoryRedis):
+    if not method_name.startswith('_') and method_name != 'pipeline':
+        setattr(Pipeline, method_name, _make_pipelined_method(method_name))
 
 
 
--- portfoliyo/tests/test_redis.py
+++ portfoliyo/tests/test_redis.py
@@ -3,7 +3,7 @@ from portfoliyo.redis import InMemoryRedis
 
 
 
-def test_redis_sets():
+def test_sets():
     """Test in-memory implementation of Redis sets."""
     c = InMemoryRedis()
     c.sadd('foo', 'bar')
@@ -15,7 +15,7 @@ def test_redis_sets():
     assert c.smembers('foo') == {'bar'}
 
 
-def test_redis_delete():
+def test_delete():
     """Test in-memory implementation of Redis delete."""
     c = InMemoryRedis()
     c.delete('foo')
@@ -25,18 +25,45 @@ def test_redis_delete():
     assert not c.sismember('foo', 'bar')
 
 
-def test_redis_incr():
+def test_incr():
     """Test in-memory implementation of Redis incr."""
     c = InMemoryRedis()
     assert c.incr('foo') == 1
     assert c.incr('foo') == 2
 
 
-def test_redis_pipeline():
-    """Test in-memory faking of redis pipelining."""
+def test_pipeline():
+    """Test in-memory implementation of Redis pipelining."""
     c = InMemoryRedis()
     p = c.pipeline()
+    p.incr('foo').incr('bar')
     p.incr('foo')
-    p.execute()
+    res = p.execute()
 
-    assert c.incr('foo') == 2
+    assert res == [1, 1, 2]
+    assert c.incr('foo') == 3
+    assert c.incr('bar') == 2
+
+
+def test_hashes():
+    """Test in-memory implementation of Redis hashes."""
+    c = InMemoryRedis()
+    c.hmset('foo', {'one': 'two', 'two': 'three'})
+    c.hmset('foo', {'one': 'three', 'four': 'five'})
+
+    assert c.hgetall('foo') == {'one': 'three', 'two': 'three', 'four': 'five'}
+
+
+def test_sorted_sets():
+    """Test in-memory implementation of Redis sorted sets."""
+    c = InMemoryRedis()
+    c.zadd('foo', 7, 'five')
+    c.zadd('foo', 5, 'five')
+    c.zadd('foo', 2, 'three')
+    c.zadd('foo', 3, 'three')
+    c.zadd('foo', 8, 'eight')
+    c.zadd('foo', 0, 0)
+
+    assert c.zrangebyscore('foo', 3, 6) == ['three', 'five']
+    assert c.zrangebyscore('foo', '-inf', '+inf') == [
+        '0', 'three', 'five', 'eight']
