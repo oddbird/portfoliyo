c5adbedefa9d4f8f0d390c203e8c28fd104fcbdf
Carl Meyer <carl@oddbird.net>
2013-01-04 12:53:28 -0700

    Reorganize notification-sending tests.

--- portfoliyo/tests/notifications/test_send.py
+++ portfoliyo/tests/notifications/test_send.py
@@ -6,52 +6,55 @@ from portfoliyo.tests import factories
 
 
 
-def test_send_only_if_email(db):
-    """If user has no email, notifications not queried or sent."""
-    p = factories.ProfileFactory.create(user__email=None)
-    with mock.patch('portfoliyo.notifications.store.get_and_clear_all') as mg:
-        with mock.patch('portfoliyo.notifications.send._send_email') as mse:
-            assert not send.send(p.id)
-
-    assert not mg.call_count
-    assert not mse.call_count
+class TestSend(object):
+    GET_AND_CLEAR_ALL = 'portfoliyo.notifications.store.get_and_clear_all'
+    SEND_EMAIL = 'portfoliyo.notifications.send._send_email'
 
 
+    def test_send_only_if_email(self, db):
+        """If user has no email, notifications not queried or sent."""
+        p = factories.ProfileFactory.create(user__email=None)
+        with mock.patch(self.GET_AND_CLEAR_ALL) as mock_get_and_clear:
+            with mock.patch(self.SEND_EMAIL) as mock_send_email:
+                assert not send.send(p.id)
 
-def test_send_only_if_active(db):
-    """If user is inactive, notifications not queried or sent."""
-    p = factories.ProfileFactory.create(
-        user__email='foo@example.com', user__is_active=False)
-    with mock.patch('portfoliyo.notifications.store.get_and_clear_all') as mg:
-        with mock.patch('portfoliyo.notifications.send._send_email') as mse:
-            assert not send.send(p.id)
-
-    assert not mg.call_count
-    assert not mse.call_count
+        assert not mock_get_and_clear.call_count
+        assert not mock_send_email.call_count
 
 
+    def test_send_only_if_active(self, db):
+        """If user is inactive, notifications not queried or sent."""
+        p = factories.ProfileFactory.create(
+            user__email='foo@example.com', user__is_active=False)
+        with mock.patch(self.GET_AND_CLEAR_ALL) as mock_get_and_clear:
+            with mock.patch(self.SEND_EMAIL) as mock_send_email:
+                assert not send.send(p.id)
 
-def test_send_only_if_notifications(db):
-    """If there are no notifications, don't try to send an email."""
-    p = factories.ProfileFactory.create(
-        user__email='foo@example.com', user__is_active=True)
-    with mock.patch('portfoliyo.notifications.store.get_and_clear_all') as mg:
-        mg.return_value = []
-        with mock.patch('portfoliyo.notifications.send._send_email') as mse:
-            assert not send.send(p.id)
-
-    assert not mse.call_count
-
+        assert not mock_get_and_clear.call_count
+        assert not mock_send_email.call_count
 
 
-def test_send_passes_profile_and_notifications_to_send_email(db):
-    """If there are notifications, pass profile and notifications on."""
-    p = factories.ProfileFactory.create(
-        user__email='foo@example.com', user__is_active=True)
-    with mock.patch('portfoliyo.notifications.store.get_and_clear_all') as mg:
-        mg.return_value = [{'name': 'foo'}]
-        with mock.patch('portfoliyo.notifications.send._send_email') as mse:
-            mse.return_value = True
-            assert send.send(p.id)
+    def test_send_only_if_notifications(self, db):
+        """If there are no notifications, don't try to send an email."""
+        p = factories.ProfileFactory.create(
+            user__email='foo@example.com', user__is_active=True)
+        with mock.patch(self.GET_AND_CLEAR_ALL) as mock_get_and_clear:
+            mock_get_and_clear.return_value = []
+            with mock.patch(self.SEND_EMAIL) as mock_send_email:
+                assert not send.send(p.id)
 
-    mse.assert_called_with(p, mg.return_value)
+        assert not mock_send_email.call_count
+
+
+
+    def test_send_passes_profile_and_notifications_to_send_email(self, db):
+        """If there are notifications, pass profile and notifications on."""
+        p = factories.ProfileFactory.create(
+            user__email='foo@example.com', user__is_active=True)
+        with mock.patch(self.GET_AND_CLEAR_ALL) as mock_get_and_clear:
+            mock_get_and_clear.return_value = [{'name': 'foo'}]
+            with mock.patch(self.SEND_EMAIL) as mock_send_email:
+                mock_send_email.return_value = True
+                assert send.send(p.id)
+
+        mock_send_email.assert_called_with(p, mock_get_and_clear.return_value)
