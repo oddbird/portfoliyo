c56aee37e2f037813ab519ae6526b17f120e599e
Carl Meyer <carl@oddbird.net>
2013-01-10 13:59:01 -0700

    Add Spanish translations of messages.

--- portfoliyo/sms/messages.py
+++ portfoliyo/sms/messages.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """SMS auto-message translations."""
 from django.conf import settings
 
@@ -14,53 +15,73 @@ def get(key, lang):
 MESSAGES = {
     'ACK_STOP': {
         'en': (
-            "No problem! Sorry to have bothered you. "
-            "Text this number anytime to re-start."
+            u"No problem! Sorry to have bothered you. "
+            u"Text this number anytime to re-start."
             ),
         },
     'ACTIVATED': {
-        'en': "You can text this number to talk with %s.",
+        'en': u"You can text this number to talk with %s.",
+        'es': u"Usted puede enviar un texto a este número para hablar con %s.",
         },
     'NO_STUDENTS': {
         'en': (
-            "Sorry, we can't find any students connected to your number, "
-            "so we're not able to deliver your message. "
-            "Please contact your student's teacher for help."
+            u"Sorry, we can't find any students connected to your number, "
+            u"so we're not able to deliver your message. "
+            u"Please contact your student's teacher for help."
             ),
         },
     'STUDENT_NAME': {
-        'en': "Thanks! What is the name of your child in %s's class?",
+        'en': u"Thanks! What is the name of your child in %s's class?",
+        'es': (
+            u"¡Gracias! "
+            u"¿Cuál es el nombre de su hijo en la clase del %s?"
+            ),
         },
     'RELATIONSHIP': {
         'en': (
-            "And what is your relationship to that child "
-            "(mother, father, ...)?"
+            u"And what is your relationship to that child "
+            u"(mother, father, ...)?"
             ),
+        'es': u"¿Y cuál es su relación con el niño (madre, padre, ...)?",
         },
     'NAME': {
-        'en': "Last question: what is your name? (So %s knows who is texting.)",
+        'en': (
+            u"Last question: "
+            u"what is your name? (So %s knows who is texting.)"
+            ),
+        'es': (
+            u"Última pregunta: ¿Cuál es su nombre? "
+            u"(Así que el %s sabe quién está enviando mensajes de texto)."
+            ),
         },
     'ALL_DONE': {
-        'en': "All done, thank you! You can text this number to talk with %s.",
+        'en': u"All done, thank you! You can text this number to talk with %s.",
+        'es': (
+            u"Resuelto, gracias! "
+            u"Usted puede enviar un texto a este número para hablar con %s.",
+            )
         },
     'SUBSEQUENT_CODE_DONE': {
-        'en': "Ok, thanks! You can text %s at this number too.",
+        'en': u"Ok, thanks! You can text %s at this number too.",
         },
     'STUDENT_NAME_FOLLOWUP': {
-        'en': "Now, what's the student's name?",
+        'en': u"And what's the student's name?",
+        'es': u"¿Y cuál es el nombre de su hijo?",
         },
     'RELATIONSHIP_FOLLOWUP': {
-        'en': "Now, what's your relationship to the student?",
+        'en': u"And what's your relationship to the student?",
+        'es': u"¿Y cuál es su relación con el niño?",
         },
     'NAME_FOLLOWUP': {
-        'en': "Now, what's your name?",
+        'en': u"And what's your name?",
+        'es': u"¿Y cuál es su nombre?",
         },
     'UNKNOWN': {
         'en': (
-            "We don't recognize your phone number, "
-            "so we don't know who to send your text to! "
-            "If you are just signing up, "
-            "make sure your invite code is typed correctly."
+            u"We don't recognize your phone number, "
+            u"so we don't know who to send your text to! "
+            u"If you are just signing up, "
+            u"make sure your invite code is typed correctly."
             ),
         },
     }
