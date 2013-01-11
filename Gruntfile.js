cd96c49542f6b9a216bccb09fa062c07c6ef55a3
Jonny Gerig Meyer <jonny@oddbird.net>
2013-01-11 12:02:46 -0500

    grunt optimize handlebars precompilation using known helpers

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         shell: {
             handlebars_compile: {
-                command: 'node_modules/.bin/handlebars jstemplates/*.handlebars -f static/js/jstemplates.js',
+                command: 'node_modules/.bin/handlebars jstemplates/*.handlebars -f static/js/jstemplates.js -k each -k if -k unless',
                 stdout: true
             }
         },
