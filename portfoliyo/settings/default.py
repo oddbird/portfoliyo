271697592356b2c5236f232f696ea3aa615095c0
Carl Meyer <carl@oddbird.net>
2013-01-18 11:36:03 -0700

    Add optional django-debug-toolbar (use DEBUG_TOOLBAR=True in settings/local.py).

--- portfoliyo/settings/default.py
+++ portfoliyo/settings/default.py
@@ -11,3 +11,13 @@ if DEBUG:
         EMAIL_BACKEND
     except NameError:
         EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+if DEBUG_TOOLBAR:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    INTERNAL_IPS = ['127.0.0.1']
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': True,
+        'HIDE_DJANGO_SQL': True,
+        }
