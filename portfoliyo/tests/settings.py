56f78adccc8b90034377433d0a69bb84c05ca9cf
Carl Meyer <carl@oddbird.net>
2013-01-18 10:40:30 -0700

    Avoid actually calling out to Pusher in tests.

--- portfoliyo/tests/settings.py
+++ portfoliyo/tests/settings.py
@@ -5,3 +5,7 @@ COMPRESS_ENABLED = False
 CELERY_ALWAYS_EAGER = True
 # avoid actually calling out to Mixpanel in tests
 MIXPANEL_ID = None
+# avoid actually calling out to Pusher in tests
+PUSHER_APPID = None
+PUSHER_KEY = None
+PUSHER_SECRET = None
