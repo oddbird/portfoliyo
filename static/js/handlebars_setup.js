004c319b6742253f226c3fb081d0c58c1f0391f6
Carl Meyer <carl@oddbird.net>
2013-01-18 10:41:49 -0700

    Clarify comment in JS.

--- static/js/handlebars_setup.js
+++ static/js/handlebars_setup.js
@@ -3,7 +3,7 @@ var PYO = (function (PYO, $) {
     'use strict';
 
     PYO.tpl = function (template, data) {
-        // This will remove any text nodes from templates.
+        // This will remove any top-level text nodes from rendered templates.
         // ...see http://bugs.jquery.com/ticket/12462 and https://github.com/wycats/handlebars.js/issues/162
         return $($.parseHTML(Handlebars.templates[template](data))).filter('*');
     };
